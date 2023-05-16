4d84bc87351adbffa82a97a4611acf553a93999c
siremir <96045277+siremir@users.noreply.github.com>
2023-05-16 02:10:57 -0500

    Added my project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import validator from './validator.js';
 
-console.log(validator);
+const inputCard = document.getElementById("cardNumber");
+inputCard.addEventListener("input", function () {
+  if (inputCard.value.length > 16) {
+    inputCard.value = inputCard.value.slice(0, 16);
+  }
+})
+
+const fullName = document.getElementById("fullName");
+const email = document.getElementById("email");
+const month = document.getElementById("month");
+const cvv = document.getElementById("cvv");
+const formulario = document.getElementById("formulario");
+const botonEnviar = document.getElementById("enviar");
+const error = document.getElementById("error");
+const thankYouTitle = document.getElementById("thankYouTitle");
+const thankYouCard = document.getElementById("thankYouCard");
+const thankYouEmail = document.getElementById("thankYouEmail");
+const thankYouP = document.getElementById("thankYouP");
+const thankYou = document.getElementById("thankYou");
+const franquicia = document.createElement("img");
+
+botonEnviar.addEventListener("click", function () {
+  if (inputCard.value && email.value && month.value && cvv.value) {
+    if (validator.isValid(inputCard.value) === false) {
+      error.innerText = "Ingrese un número de tarjeta válido";
+    }
+    if (validator.isValid(inputCard.value) === true) {
+      thankYou.style.backgroundColor = "white";
+      thankYou.style.margin = "0 auto";
+      thankYou.style.width = "700px";
+      thankYou.style.borderRadius = "10px";
+      thankYou.style.fontWeight = "400";
+      thankYou.style.paddingTop = "30px";
+      thankYou.style.paddingBottom = "80px";
+      thankYouTitle.innerText = "¡Gracias por tu compra " + fullName.value + "!";
+      thankYouCard.innerText = "Tarjeta: " + validator.maskify(inputCard.value);
+      thankYouCard.append(franquicia);
+      franquicia.src = validator.franquicia(inputCard.value);
+      franquicia.classList.add("imgCard");
+      thankYouEmail.innerText = "Email: " + email.value;
+      thankYouP.innerText = "Te enviaremos los detalles de tu pedido al correo ingresado.";
+      error.innerText = "";
+      formulario.style.display = "none";
+    }
+  } else error.innerText = "*Se requiere llenar todos los campos";
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
 const validator = {
-  // ...
+  isValid: function (number) {
+    let creditNumber = number;
+    if (creditNumber !== "") {
+      creditNumber = Array.from(creditNumber);
+      creditNumber.reverse();
+      for (let i = 0; i < creditNumber.length; i++) {
+        if (i % 2 !== 0) {
+          creditNumber[i] = creditNumber[i] * 2;
+          creditNumber[i] = creditNumber[i].toString();
+          if (creditNumber[i] >= 10) {
+            creditNumber[i] = parseInt(creditNumber[i][0]) + parseInt(creditNumber[i][1]);
+          } else creditNumber[i] = parseInt(creditNumber[i]);
+        } else creditNumber[i] = parseInt(creditNumber[i]);
+      }
+      creditNumber = creditNumber.reduce((a, b) => {
+        return a + b
+      });
+      if (creditNumber % 10 === 0) {
+        return true;
+      } else return false;
+    }
+  },
+  maskify: function (number) {
+    let creditNumber = number;
+    if (creditNumber.length > 4) {
+      creditNumber = "#".repeat(creditNumber.length - 4) + creditNumber.slice(-4);
+    }
+    return creditNumber;
+  },
+  franquicia: function (number) {
+    if (number[0] === "4") {
+      return "./images/visa.png";
+    } else if (number[0] === "3") {
+      return "./images/amex.png";
+    } else if (number[0] === "5") {
+      return "./images/master.png";
+    } else return "./images/diners.png";
+  }
 };
 
 export default validator;
